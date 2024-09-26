ffbca442620ce75cdf25c0bee092497b29988bf9
KarolKoniewicz <kokarolos@gmail.com>
2024-09-26 19:29:21 +0300

    added some charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@/components/ui/button";
+import { MultipleChart } from "@/components/ui/MultipleChart";
+import { CustomPieChart } from "@/components/ui/CustomPieChart";
+import { ChartBarInteractive } from "@/components/ChartBarInteractive";
 
 export default function Home() {
   return (
-    <Button> Click Me</Button>    
+    <>
+      <div className="flex pt-2 justify-center space-x-4">
+        <div className="p-2 bg-gray-200">
+          <MultipleChart></MultipleChart>
+        </div>
+
+        <div className="p-2 bg-gray-200">
+          <CustomPieChart></CustomPieChart>
+        </div>
+      </div>
+
+      <div className="flex pt-2 justify-center space-x-4">
+        <ChartBarInteractive></ChartBarInteractive>
+      </div>
+    </>
   );
 }
