6131b48d79e5123ab5149645b591091f4796afed
KarolKoniewicz <kokarolos@gmail.com>
2024-09-27 13:20:02 +0300

    added more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
 import { MultipleChart } from "@/components/ui/MultipleChart";
 import { CustomPieChart } from "@/components/ui/CustomPieChart";
-import { ChartBarInteractive } from "@/components/ChartBarInteractive";
+import { ChartBarInteractive } from "@/components/ui/ChartBarInteractive";
+import { ChartLine } from "@/components/ui/ChartLine";
+import { ChartBarNegative } from "@/components/ui/ChartBarNegative";
+import { useEffect } from 'react';
+import { CustomRadarChart } from "@/components/ui/CustomRadarChart";
+import { RadarChartLabel } from "@/components/ui/RadarChartLabel";
+import { RadialChart } from "@/components/ui/RadialChart";
 
 export default function Home() {
+
+
+  useEffect(() => {
+    const userLocale = navigator.language || 'en-US';
+  }, []);
+
+
   return (
     <>
       <div className="flex pt-2 justify-center space-x-4">
@@ -12,6 +27,27 @@ export default function Home() {
 
         <div className="p-2 bg-gray-200">
           <CustomPieChart></CustomPieChart>
+        </div>
+
+        <div className="p-2 bg-gray-200">
+          <ChartBarNegative></ChartBarNegative>
+        </div>
+
+        <div className="p-2 bg-gray-200">
+          <ChartLine></ChartLine>
+        </div>
+      </div>
+
+
+      <div className="flex pt-2 justify-center space-x-4">
+        <div className="p-2 bg-gray-200">
+          <CustomRadarChart></CustomRadarChart>
+        </div>
+        <div className="p-2 bg-gray-200">
+          <RadarChartLabel></RadarChartLabel>
+        </div>
+        <div className="p-2 bg-gray-200">
+          <RadialChart></RadialChart>
         </div>
       </div>
 
